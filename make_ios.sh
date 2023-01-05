94329943bf1acf364b68d42b7f5cfe53f7e2a688
shenpeng.zhang <shenpeng.zhang@shopee.com>
2023-01-05 18:12:57 +0800

    vmaf ios compile

---
+++ make_ios.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+BASE_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+LIBVMAF_PATH=${BASE_PATH}/vmaf/libvmaf
+LIBVMAF_INSTALL=${BASE_PATH}/install
+pushd ${LIBVMAF_PATH}
+
+git reset --hard v2.2.1
+
+# prepare
+python3 -m pip install virtualenv
+python3 -m virtualenv .venv
+source .venv/bin/activate
+pip install meson
+
+meson setup --prefix ${BASE_PATH}/build-ios-armv7 --cross-file ${BASE_PATH}/iphone_armv7.txt build-ios-armv7
+ninja -vC build-ios-armv7 install
+
+meson setup --prefix ${BASE_PATH}/build-ios-arm64 --cross-file ${BASE_PATH}/iphone_arm64.txt build-ios-arm64
+ninja -vC build-ios-arm64 install
+
+popd
+
+if [ ! -d $LIBVMAF_INSTALL  ]; then
+  mkdir $LIBVMAF_INSTALL
+  mkdir $LIBVMAF_INSTALL/include
+  mkdir $LIBVMAF_INSTALL/lib
+fi
+
+cp -r ${BASE_PATH}/build-ios-arm64/include ${LIBVMAF_INSTALL}/include
+lipo -create ${BASE_PATH}/build-ios-arm64/lib/libvmaf.a ${BASE_PATH}/build-ios-armv7/lib/libvmaf.a -output ${LIBVMAF_INSTALL}/lib/libvmaf.a
